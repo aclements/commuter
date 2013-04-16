6b5605be1d52bb0745bb045767990d533a40b394
Nickolai Zeldovich <nickolai@csail.mit.edu>
2013-04-16 04:29:06 -0400

    better parallel mtrace glue for xv6
    Avoids qemu screwing up the tty

--- par-mtrace-xv6.py
+++ par-mtrace-xv6.py
@@ -6,15 +6,16 @@ ncpu = multiprocessing.cpu_count()
 
 os.system("make HW=mtrace mscan.kern")
 
+null = open('/dev/null', 'rw')
 procs = []
 for n in range(0, ncpu):
-  out = open('qemu.out.%d' % n, 'w')
   args = ["make", "HW=mtrace",
                   "MTRACEOUT=mtrace.out.%d" % n,
                   "RUN=fstest -t -n %d -p %d; halt" % (ncpu, n),
                   "QEMUNOREDIR=x",
+                  "QEMUOUTPUT=qemu.out.%d" % n,
                   "mtrace.out.%d-scripted" % n]
-  p = subprocess.Popen(args, stdout=out, stderr=subprocess.STDOUT)
+  p = subprocess.Popen(args, stdout=null, stdin=null, stderr=null)
   procs.append(p)
 
 for p in procs:

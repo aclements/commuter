1daceb122807fdc746d3909352cf28fc9ba016a5
Austin Clements <amdragon@mit.edu>
2013-08-01 01:18:15 -0400

    Simpler Linux fstest setup based on initramfs
    The old setup involved booting a Debian disk image and a user-space
    NFS server.  This new setup is entirely disk-less and network-less,
    booting instead off an initramfs containing an xv6 user space.  As a
    result, we no longer need the various Linux support scripts, and we
    can reuse a lot of the xv6 mtrace infrastructure.
    
    This also makes one change to the Linux kernel configuration,
    switching the default HZ=1000 to HZ=100.  Without this, I found that
    Linux would essentially live-lock taking timer interrupts, causing
    each test to take about a minute.  This tweak makes each test take
    less than a second.

--- par-mtrace-linux.py
+++ par-mtrace-linux.py
@@ -4,34 +4,20 @@ import os, multiprocessing, subprocess, signal
 
 ncpu = multiprocessing.cpu_count()
 
-nfsd = subprocess.Popen(["./nfs-server.sh", "/tmp/nfs-export"])
+os.system("make HW=linuxmtrace")
 
 null = open('/dev/null', 'rw')
 procs = []
-disks = []
 for n in range(0, ncpu):
-  disk = '/mnt/tmpfs/cow-%d.img' % n
-  subprocess.call(['qemu-img', 'create',
-                   '-f', 'qcow2',
-                   '-b', '%s/fs.img' % os.getcwd(),
-                   disk])
-  disks.append(disk)
-
-  args = ["./mtrace-scripted.sh",
-          "mtrace.out.%d" % n,
-          disk,
-          "qemu.out.%d" % n,
-          "fstest_parts=%d" % ncpu,
-          "fstest_thispart=%d" % n]
+  args = ["make", "HW=linuxmtrace",
+                  "KERN=../pk/arch/x86_64/boot/bzImage",
+                  "MTRACEOUT=mtrace.out.%03d" % n,
+                  "RUN=fstest -t -n %d -p %d; halt" % (ncpu, n),
+                  "QEMUNOREDIR=x",
+                  "QEMUOUTPUT=qemu.out.%03d" % n,
+                  "mtrace.out.%03d-scripted" % n]
   p = subprocess.Popen(args, stdout=null, stdin=null, stderr=null)
   procs.append(p)
 
 for p in procs:
   p.wait()
-
-for d in disks:
-  os.unlink(d)
-
-os.kill(nfsd.pid, signal.SIGTERM)
-nfsd.wait()
-

9d40a8fa3d2cf890ac47e0ef46157c61e4891358
Austin Clements <amdragon@mit.edu>
2013-08-01 01:03:01 -0400

    par-mscan: Direct mscan output to a file to avoid blocking
    Previously, we had mscan output to a pipe, but that would block mscan
    until the Python combining code got around to reading from it.  By the
    time mscan started writing, it was mostly done, but not entirely done,
    so this limited par-mscan's parallelism at the end.  Instead, redirect
    its output to a file to avoid blocking mscan.  This noticeably
    improves the performance of the tail of par-mscan.

--- par-mscan.py
+++ par-mscan.py
@@ -4,20 +4,18 @@ import subprocess, glob, json, os, sys
 
 procs = []
 for mtraceout in sorted(glob.glob('mtrace.out.*')):
+  mscanout = 'mscan.out.' + mtraceout[len('mtrace.out.'):]
   args = ["../mtrace/mtrace-tools/mscan",
           "--mtrace-log-file=%s" % mtraceout,
           "--check-testcases"] + sys.argv[1:]
-  p = subprocess.Popen(args, stdout=subprocess.PIPE)
-  procs.append(p)
+  p = subprocess.Popen(args, stdout=file(mscanout, 'w'))
+  procs.append((p, args, mscanout))
 
 merged_json = { 'testcases': [] }
-for i, p in enumerate(procs):
-  s = p.stdout.read()
-  try:
-    d = json.loads(s)
-  except ValueError, e:
-    raise ValueError(str(e) + " from mtrace shard %03d" % i)
+for i, (proc, args, mscanout) in enumerate(procs):
+  if proc.wait():
+    raise subprocess.CalledProcessError(proc.returncode, args)
+  d = json.load(file(mscanout))
   merged_json['testcases'].extend(d['testcases'])
-  p.wait()
 
 print json.dumps(merged_json, indent=2)

1ad93319967100bfb2c0bf26023652ba5890050b
Austin Clements <amdragon@mit.edu>
2013-08-02 11:33:00 -0400

    par-mtrace/par-mscan: Take log directory and jobs on command line

--- par-mscan.py
+++ par-mscan.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python
 
-import subprocess, glob, json, os, sys
+import subprocess, glob, json, os, sys, argparse
+
+parser = argparse.ArgumentParser(
+  epilog='Unknown arguments will be passed to mscan')
+parser.add_argument('-d', '--log-dir', metavar='DIR', default='.',
+                    help='Read logs from and write output shards to DIR')
+args, mscanargs = parser.parse_known_args()
 
 procs = []
-for mtraceout in sorted(glob.glob('mtrace.out.*')):
-  mscanout = 'mscan.out.' + mtraceout[len('mtrace.out.'):]
-  args = ["../mtrace/mtrace-tools/mscan",
+for mtraceout in sorted(glob.glob(os.path.join(args.log_dir, 'mtrace.out.*'))):
+  mscanout = os.path.join(
+    args.log_dir, 'mscan.out.' + mtraceout.rpartition('mtrace.out.')[2])
+  cmd = ["../mtrace/mtrace-tools/mscan",
           "--mtrace-log-file=%s" % mtraceout,
-          "--check-testcases"] + sys.argv[1:]
-  p = subprocess.Popen(args, stdout=file(mscanout, 'w'))
-  procs.append((p, args, mscanout))
+          "--check-testcases"] + mscanargs
+  p = subprocess.Popen(cmd, stdout=file(mscanout, 'w'))
+  procs.append((p, cmd, mscanout))
 
 merged_json = { 'testcases': [] }
-for i, (proc, args, mscanout) in enumerate(procs):
+for i, (proc, cmd, mscanout) in enumerate(procs):
   if proc.wait():
-    raise subprocess.CalledProcessError(proc.returncode, args)
+    raise subprocess.CalledProcessError(proc.returncode, cmd)
   d = json.load(file(mscanout))
   merged_json['testcases'].extend(d['testcases'])
 
--- par-mtrace-xv6.py
+++ par-mtrace-xv6.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python
 
-import os, multiprocessing, subprocess
+import os, multiprocessing, subprocess, argparse
 
-ncpu = multiprocessing.cpu_count()
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--log-dir', metavar='DIR', default='.',
+                    help='Store output logs in DIR')
+parser.add_argument('-j', '--jobs', type=int,
+                    default=multiprocessing.cpu_count(),
+                    help='Number of parallel jobs')
+args = parser.parse_args()
 
 os.system("make HW=mtrace")
 
 null = open('/dev/null', 'rw')
 procs = []
-for n in range(0, ncpu):
-  args = ["make", "HW=mtrace",
-                  "MTRACEOUT=mtrace.out.%03d" % n,
-                  "RUN=fstest -t -n %d -p %d; halt" % (ncpu, n),
-                  "QEMUNOREDIR=x",
-                  "QEMUOUTPUT=qemu.out.%03d" % n,
-                  "mtrace.out.%03d-scripted" % n]
-  p = subprocess.Popen(args, stdout=null, stdin=null)
+for n in range(0, args.jobs):
+  cmd = ["make",
+         "HW=mtrace",
+         "MTRACEOUT=" + os.path.join(args.log_dir, "mtrace.out.%03d" % n),
+         "RUN=fstest -t -n %d -p %d; halt" % (args.jobs, n),
+         "QEMUNOREDIR=x",
+         "QEMUOUTPUT=" + os.path.join(args.log_dir, "qemu.out.%03d" % n),
+         os.path.join(args.log_dir, "mtrace.out.%03d-scripted" % n)]
+  p = subprocess.Popen(cmd, stdout=null, stdin=null)
   procs.append(p)
 
 for p in procs:

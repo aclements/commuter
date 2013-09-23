d1b86f2f8982e0c7c2de19010deeb8393ae4946f
Austin Clements <amdragon@mit.edu>
2013-09-23 02:23:12 -0400

    models: Tidy counter toy model

--- models/counter.py
+++ models/counter.py
@@ -2,23 +2,21 @@ import simsym
 import model
 
 class Counter(simsym.tstruct(counter=simsym.SInt)):
-    __slots__ = ["counter"]
-
     def _declare_assumptions(self, assume):
         super(Counter, self)._declare_assumptions(assume)
         assume(self.counter >= 0)
 
     @model.methodwrap()
-    def sys_inc(self):
+    def inc(self):
         self.counter = self.counter + 1
 
     @model.methodwrap()
-    def sys_dec(self):
+    def dec(self):
         simsym.assume(self.counter > 0)
         self.counter = self.counter - 1
 
     @model.methodwrap()
-    def sys_iszero(self):
+    def iszero(self):
         return self.counter == 0
 
 model_class = Counter

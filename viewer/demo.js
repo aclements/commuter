3db1989babf0620ad3e348c8fdf352306744bdf5
Austin Clements <amdragon@mit.edu>
2013-10-11 11:26:48 -0400

    viewer: Comments in demo.js

--- viewer/demo.js
+++ viewer/demo.js
@@ -6,12 +6,15 @@ $(document).ready(function() {
     qc.heatmap(Predicate.conflicted,
                function(tc) { return tc.runid; });
     qc.heatbar(Predicate.conflicted);
+
+    // Disable the following when not using separate details databases
     qc.table(function(tc) {
         // Lazy load detail databases
         if (!database.loadMscan('data/' + tc.runid + '-details.json'))
             return $('<span>').text('Loading details...');
     });
 
+    // Load main mscan databases
     database.loadMscan('data/sv6.json');
     database.loadMscan('data/Linux.json');
 });
